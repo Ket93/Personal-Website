3a90364597c12a5129254468d1ba4f4ef0c18c38
Ket93 <38818395+Ket93@users.noreply.github.com>
2021-07-16 15:55:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ debateImg.addEventListener("click", function () {
     Swal.fire({
         showCloseButton: true,
         showConfirmButton: false,
-        imageUrl: 'debateLogo.png',
+        imageUrl: 'Images/debateLogo.png',
         imageWidth: 330,
         imageHeight: 250,
         title: '<b>Debate  2017-2021<b>',
